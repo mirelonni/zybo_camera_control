1caa803e9b14a04538e8a7543e50382f220e12ee
Mircea Cojocaru <mirelonni@gmail.com>
2019-04-19 01:56:48 +0300

    cleaned repo

--- opencvcarctrl/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lopencv_core -lopencv_objdetect -lm -lopencv_imgcodecs -lopencv_imgproc
-
